41d9d6d724741b69c854cc2f876c98b4defe48ad
KaioZaniboni <blackdinamo@hotmail.com>
2023-04-18 04:49:45 -0300

    teste de  remoção

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -14,7 +14,6 @@ public class APITest {
 		RestAssured.baseURI = "http://localhost:8001/tasks-backend/";
 	}
 	
-	
 	@Test
 	public void deveRetornarTarefas() {
 		RestAssured.given()
@@ -46,4 +45,27 @@ public class APITest {
 		.statusCode(400)
 		.body("message", CoreMatchers.is("Due date must not be in past"));
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+			// inserir uma tarefa
+			Integer id = RestAssured.given()
+			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2033-04-13\" }")
+			.contentType(ContentType.JSON)
+			.when()
+			.post("/todo")
+			.then()
+			.statusCode(201)
+			.extract().path("id");
+			
+			System.out.println(id);
+			
+			// remover a tarefa
+			RestAssured.given()
+			.when()
+			.delete("/todo/"+id)
+			.then()
+			.statusCode(204);
+	}
+
 }
